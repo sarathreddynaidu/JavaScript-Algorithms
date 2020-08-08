b3882073ed9ca0b15fcc1ee653c1917e3b0aeca0
Sarath Chandra Reddy Naidu <sarathreddynaidu@gmail.com>
2020-08-08 18:16:44 -0500

    added bubble sort algorithm

---
+++ BubbleSort.js
@@ -0,0 +1,20 @@
+function swap(arr, idx1, idx2) {
+    let temp = arr[idx1];
+    arr[idx1] = arr[idx2];
+    arr[idx2] = temp;
+}
+
+function bubbleSort(arr) {
+    let noSwaps;
+    for (let i = arr.length; i > 0; i--) {
+        noSwaps = true;
+        for (let j = 0; j < i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1);
+                noSwaps = false;
+            }
+        }
+        if (noSwaps) break;
+    }
+    return arr;
+}
